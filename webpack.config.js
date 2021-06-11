a872a549267f08d0cea21939ecd9b166fb944ffa
Lubos Marton <lubos.marton@accenture.com>
2021-06-11 16:28:27 +0200

    5

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,11 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
   plugins: [
