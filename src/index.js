3c1e9f0dbc713ebb90181a01cae7d1ece80ff52d
Lubos Marton <lubos.marton@accenture.com>
2021-06-11 16:05:36 +0200

    second

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App"
 
-const App = ({firstName, lastName}) => {
-  return (
-    <>
-      <h1 className="">{firstName}</h1>
-      <h2 className="">{lastName}</h2>
-    </>
-  );
-};
-ReactDOM.render(<App firstName="Lubos" lastName="Marton" />, 
-document.getElementById("root"));
+ReactDOM.render
+
