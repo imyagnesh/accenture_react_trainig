669e4929ef1605f211a1d093215889d022e60b71
Lubos Marton <lubos.marton@accenture.com>
2021-06-11 16:10:33 +0200

    4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App"
+import App from "./App";
 
-ReactDOM.render
+ReactDOM.render(
+  <>
+    <App firstName="Lubos" lastName="Marton" />
+    <App firstName="rohit" lastName="sharma" />
+    <App firstName="virat" lastName="kohli" />
+  </>,
+  document.getElementById("root")
+);
