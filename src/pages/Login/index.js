1f9bb4c8bd3fd35f94c3907a01508dba6a1c0fac
Stefan <stefanut-george.dudu@accenture.com>
2021-06-15 15:52:36 +0200

    Confirmed password done

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -13,21 +13,26 @@ class Login extends Component {
         <h1>Login Page</h1>
         <Formik
           initialValues={{
+            name:'',
             username: '',
             password: '',
+            passwordConf: ''
           }}
           validate={(values) => {
             const error = {};
+            if(!values.name) {
+              error.name = "Please Enter a valid Name"
+          }
             if(!values.username) {
                 error.username = "Please Enter Username"
             }
-            if(!values.password) {
-                error.password = "Please Enter Password"
+            if(values.password !== values.passwordConf) {
+                error.password = "The passwords do not match"
             }
             return error;
           }}
           onSubmit={async (values, actions) => {
-            await wait(5000);
+            await wait(2000);
             console.log(values);
           }}
         >
@@ -42,6 +47,11 @@ class Login extends Component {
           }) => (
             <form onSubmit={handleSubmit}>
               <div>
+              <label htmlFor="name">Name</label>
+              <input type="text" name="name" value={values.name} onChange={handleChange} onBlur={handleBlur} />
+              {touched.name && errors.name && <span style={{ color: 'red'}}>{errors.name}</span>}
+              </div>
+              <div>
                 <label htmlFor="username">Username</label>
                 <input type="text" name="username" value={values.username} onChange={handleChange} onBlur={handleBlur} />
                 {touched.username && errors.username && <span style={{ color: 'red'}}>{errors.username}</span>}
@@ -49,7 +59,14 @@ class Login extends Component {
               <div>
                 <label htmlFor="password">Password</label>
                 <input type="password" name="password" value={values.password} onChange={handleChange} onBlur={handleBlur} />
-                {touched.password && errors.password && <span style={{ color: 'red'}}>{errors.password}</span>}
+                {touched.password 
+                // && errors.password && <span style={{ color: 'red'}}>{errors.password}</span>
+                }
+              </div>
+              <div>
+                <label htmlFor="passwordConf">Confirm password</label>
+                <input type="password" name="passwordConf" value={values.passwordConf} onChange={handleChange} onBlur={handleBlur} />
+                {touched.passwordConf && errors.password && <span style={{ color: 'red'}}>{errors.password}</span>}
               </div>
               <div>
                 <button disabled={isSubmitting} type="submit">Login</button>
