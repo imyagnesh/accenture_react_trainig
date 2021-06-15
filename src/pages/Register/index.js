cbe709708eb16f780331bbcea1f994ac5d6fb444
Lubos Marton <lubos.marton@accenture.com>
2021-06-15 16:21:31 +0200

    register1

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { Link } from 'react-router-dom';
+import { Formik } from 'formik';
 
-const Register = () => (
+class Register extends Component {
+
+render () {
+   return (
+ <Formik initialValues= {{
+   name:"",
+   username:"",
+   password:"",
+   confirmPassword:"",
+ }}> 
+   {({ values,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting,
+                errors,
+                touched})=>{
+    return <form onSubmit={handleSubmit}>
   <div>
-    <h1>Register pae</h1>
+    <label>name</label>
+    <input type="text" name="name" value={values.name} onChange={handleChange} onBlur={handleBlur} />
   </div>
-);
+  <div>
+    <label>username</label>
+    <input type="text" name="username" value={values.username} onChange={handleChange} onBlur={handleBlur} />
+  </div>
+  <div>
+    <label>password</label>
+    <input type="password" name="password" value={values.password} onChange={handleChange} onBlur={handleBlur} />
+  </div>
+  <div>
+    <label>corfirm password</label>
+    <input type="password" name="confirmPassword" value={values.confirmPassword} onChange={handleChange} onBlur={handleBlur} />
+  </div>
+  <button type="submit">Submit</button>
+</form>
+   }}
+
+</Formik>
+  )
+}
+}
 
 export default Register;
