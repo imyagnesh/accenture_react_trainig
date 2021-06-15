99d9ab91d25cb3d7f03fbb6c160dff5296b70d8d
Nicoleta Ungureanu <nicoleta7u@yahoo.com>
2021-06-15 10:38:33 +0200

    delete + all btn

--- src/Todo/index.js
+++ src/Todo/index.js
@@ -40,6 +40,44 @@ export class index extends Component {
     });
   };
 
+  deleteItem = (todo) => {
+    const { todoList } = this.state;
+    const index = todoList.findIndex((x) => x.id === todo.id);
+    const newList = [
+      ...todoList.slice(0, index),
+      ...todoList.slice(index + 1),
+    ];
+    this.setState({
+      todoList: newList,
+    });
+  }
+
+  selectAll = () => {
+    const { todoList } = this.state;
+    const newList = [];
+    todoList.forEach((item) => {
+      if(!item.isDone) {
+        item.isDone = true;
+      } else {
+        item.isDone = false;
+      }
+      newList.push(item);
+    });
+    this.setState({todoList: newList});
+  }
+
+  deleteSelected = () => {
+    const { todoList } = this.state;
+    todoList.forEach((item) => {
+      if(item.isDone) {
+        
+      } else {
+        item.isDone = false;
+      }
+      newList.push(item);
+    });
+  }
+
   render() {
     const { todoList } = this.state;
 
@@ -51,6 +89,7 @@ export class index extends Component {
           <button type="button" onClick={this.addTodo}>
             Add Todo
           </button>
+          <button type="button" onClick={()=>{this.deleteSelected()}}>Delete</button>
         </div>
         <div>
           {todoList.map((todo) => {
@@ -62,9 +101,15 @@ export class index extends Component {
                   onChange={() => this.completeTodo(todo)}
                 />
                 <span>{todo.todoText}</span>
+                <button type="button" onClick={() => {this.deleteItem(todo)}}>Delete</button>
               </div>
             );
           })}
+        </div>
+        <div>
+          <button type="button" onClick={() => {this.selectAll()}}>Add</button>
+          <button type="button">Pending</button>
+          <button type="button">Completed</button>
         </div>
       </div>
     );
