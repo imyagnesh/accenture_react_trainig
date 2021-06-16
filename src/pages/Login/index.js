0d1c71f0524e4aaae115379e1b593e682efac064
luana-david <andreea.luana.david@gmail.com>
2021-06-16 10:54:42 +0200

    replace formik with MyForm

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,6 +2,8 @@
 import React, { Component, createRef } from 'react';
 import { Link } from 'react-router-dom';
 import { Formik } from 'formik';
+import fields from './fields'
+import MyForm from '../../components/MyForm';
 
 const wait = (time) => new Promise(resolve => setTimeout(resolve, 5000));
 
@@ -11,57 +13,7 @@ class Login extends Component {
     return (
       <div>
         <h1>Login Page</h1>
-        <Formik
-          initialValues={{
-            username: '',
-            password: '',
-          }}
-          validate={(values) => {
-            const error = {};
-            if(!values.username) {
-                error.username = "Please Enter Username"
-            }
-            if(!values.password) {
-                error.password = "Please Enter Password"
-            }
-            
-            return error;
-          }}
-          onSubmit={async (values, actions) => {
-            await wait(5000);
-            console.log(values);
-          }}
-        >
-          {({
-                values,
-                handleChange,
-                handleBlur,
-                handleSubmit,
-                isSubmitting,
-                errors,
-                touched
-          }) => (
-            <form onSubmit={handleSubmit}>
-              <div>
-                <label htmlFor="username">Username</label>
-                <input type="text" name="username" value={values.username} onChange={handleChange} onBlur={handleBlur} />
-                {touched.username && errors.username && <span style={{ color: 'red'}}>{errors.username}</span>}
-              </div>
-              <div>
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" value={values.password} onChange={handleChange} onBlur={handleBlur} />
-                {touched.password && errors.password && <span style={{ color: 'red'}}>{errors.password}</span>}
-              </div>
-              <div>
-                <button disabled={isSubmitting} type="submit">Login</button>
-                <button disabled={isSubmitting} type="reset">Reset</button>
-              </div>
-              <div>
-                <Link to="/register">don't have accout? Please Register</Link>
-              </div>
-            </form>
-          )}
-        </Formik>
+        <MyForm fields={fields} onSubmit={(values) => console.log(values)} />
       </div>
     );
   }
