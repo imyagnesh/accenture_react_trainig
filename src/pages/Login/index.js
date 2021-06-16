1ec0581d8a3d582c6a3f303db169f6b0226c03c8
Stefan <stefanut-george.dudu@accenture.com>
2021-06-16 10:55:21 +0200

    login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,84 +1,16 @@
-/* eslint-disable */
-import React, { Component, createRef } from 'react';
-import { Link } from 'react-router-dom';
-import { Formik } from 'formik';
 
-const wait = (time) => new Promise(resolve => setTimeout(resolve, 5000));
+import React, { Component } from 'react';
+import MyForm from '../../components/MyForm'
+import fields from './fields';
 
 class Login extends Component {
-
   render() {
     return (
       <div>
-        <h1>Login Page</h1>
-        <Formik
-          initialValues={{
-            name:'',
-            username: '',
-            password: '',
-            passwordConf: ''
-          }}
-          validate={(values) => {
-            const error = {};
-            if(!values.name) {
-              error.name = "Please Enter a valid Name"
-          }
-            if(!values.username) {
-                error.username = "Please Enter Username"
-            }
-            if(values.password !== values.passwordConf) {
-                error.password = "The passwords do not match"
-            }
-            
-            return error;
-          }}
-          onSubmit={async (values, actions) => {
-            await wait(2000);
-            console.log(values);
-          }}
-        >
-          {({
-                values,
-                handleChange,
-                handleBlur,
-                handleSubmit,
-                isSubmitting,
-                errors,
-                touched
-          }) => (
-            <form onSubmit={handleSubmit}>
-              <div>
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" value={values.name} onChange={handleChange} onBlur={handleBlur} />
-              {touched.name && errors.name && <span style={{ color: 'red'}}>{errors.name}</span>}
-              </div>
-              <div>
-                <label htmlFor="username">Username</label>
-                <input type="text" name="username" value={values.username} onChange={handleChange} onBlur={handleBlur} />
-                {touched.username && errors.username && <span style={{ color: 'red'}}>{errors.username}</span>}
-              </div>
-              <div>
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" value={values.password} onChange={handleChange} onBlur={handleBlur} />
-                {touched.password 
-                // && errors.password && <span style={{ color: 'red'}}>{errors.password}</span>
-                }
-              </div>
-              <div>
-                <label htmlFor="passwordConf">Confirm password</label>
-                <input type="password" name="passwordConf" value={values.passwordConf} onChange={handleChange} onBlur={handleBlur} />
-                {touched.passwordConf && errors.password && <span style={{ color: 'red'}}>{errors.password}</span>}
-              </div>
-              <div>
-                <button disabled={isSubmitting} type="submit">Login</button>
-                <button disabled={isSubmitting} type="reset">Reset</button>
-              </div>
-              <div>
-                <Link to="/register">don't have accout? Please Register</Link>
-              </div>
-            </form>
-          )}
-        </Formik>
+          <h1>Login Page</h1>
+          <MyForm fields={fields} onSubmit={(values) => {
+              console.log(values);
+          }} />
       </div>
     );
   }
