5c8dfbda4ea52fca54bab17039ae22d41ef611b4
Mitanshu Patel <mitanshu.u.patel@gmail.com>
2021-06-17 13:02:43 +0530

    commit code

--- src/Todo/index.js
+++ src/Todo/index.js
@@ -6,6 +6,7 @@ export class Todo extends Component {
   state = {
     todoList: [],
     filterType: "all",
+    selectedItems: [],
   };
 
   async componentDidMount() {
@@ -44,12 +45,22 @@ export class Todo extends Component {
     );
   };
 
-  completeTodo = (todo) => {
+  completeTodo = async (todo) => {
     const { todoList } = this.state;
+    const res = await fetch(`http://localhost:8080/todoList/${todo.id}`, {
+      method: "PUT",
+      body: JSON.stringify({ ...todo, isDone: !todo.isDone }),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    const updatedTodo = await res.json();
+
     const index = todoList.findIndex((x) => x.id === todo.id);
     const newList = [
       ...todoList.slice(0, index),
-      { ...todo, isDone: !todo.isDone },
+      updatedTodo,
       ...todoList.slice(index + 1),
     ];
     this.setState({
@@ -57,8 +68,11 @@ export class Todo extends Component {
     });
   };
 
-  deleteTodo = (todo) => {
+  deleteTodo = async (todo) => {
     const { todoList } = this.state;
+    await fetch(`http://localhost:8080/todoList/${todo.id}`, {
+      method: "DELETE",
+    });
     const index = todoList.findIndex((x) => x.id === todo.id);
     const newList = [...todoList.slice(0, index), ...todoList.slice(index + 1)];
     this.setState({
@@ -67,7 +81,7 @@ export class Todo extends Component {
   };
 
   render() {
-    const { todoList, filterType } = this.state;
+    const { todoList, filterType, selectedItems } = this.state;
 
     return (
       <div>
@@ -82,6 +96,26 @@ export class Todo extends Component {
           <button type="submit">Add Todo</button>
         </form>
         <div>
+          <button
+            type="button"
+            onClick={async () => {
+              const promise = [];
+              for (let i = 0; i < selectedItems.length; i++) {
+                const element = selectedItems[i];
+                promise.push(
+                  fetch(`http://localhost:8080/todoList/${element}`, {
+                    method: "DELETE",
+                  })
+                );
+              }
+              await Promise.allSettled(promise);
+              this.setState({
+                todoList: todoList.filter((x) => !selectedItems.includes(x.id)),
+              });
+            }}
+          >
+            Delete Selected Todos
+          </button>
           {todoList
             .filter((todo) => {
               if (filterType === "pending") {
@@ -94,6 +128,23 @@ export class Todo extends Component {
             })
             .map((todo) => (
               <div data-testid="todo-list" key={todo.id}>
+                <input
+                  type="checkbox"
+                  onChange={(event) => {
+                    console.log(event.target.checked);
+                    if (event.target.checked) {
+                      this.setState({
+                        selectedItems: [...selectedItems, todo.id],
+                      });
+                    } else {
+                      this.setState({
+                        selectedItems: selectedItems.filter(
+                          (x) => x !== todo.id
+                        ),
+                      });
+                    }
+                  }}
+                />
                 <input
                   type="checkbox"
                   checked={todo.isDone}
